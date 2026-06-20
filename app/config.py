b983fcfc49c292ee97744865bb17513ad038c332
toolesstes <toolsgest@gmail.com>
2026-06-20 13:18:38 +0530

    update

--- app/config.py
+++ app/config.py
@@ -20,9 +20,10 @@ ICC_CHANNEL_ID = "UUBZy7bU6CkXk9wYpF8KkKXg"
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 CRICKET_API_KEYS = [
+    "971f063e-80e8-4d22-b32c-96cc4478c865",
     "e7307705-0d2d-4bdb-827f-1320066359a2",
     "971f063e-80e8-4d22-b32c-96cc4478c865",
-    "971f063e-80e8-4d22-b32c-96cc4478c865"
+    # "971f063e-80e8-4d22-b32c-96cc4478c865"
 ]
 
 # CRICKET_API_BASE_URL = "https://api.cricketdata.org/v1"
